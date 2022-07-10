cd6ef3730a4d2978c627c07234d02f85723ec0e4
DavorMajdandzic <davormajmusic@gmail.com>
2022-07-09 21:39:04 -0400

    Added subreddits and commands

--- reddit/constants.py
+++ reddit/constants.py
@@ -20,8 +20,26 @@ NOT_EMBED_DOMAINS = ("gfycat.com/", "gifdeliverynetwork.com/", "redgifs.com", "i
 GOOD_EXTENSIONS = (".png", ".jpg", ".jpeg", ".gif", "gifv")
 
 # Main Subreddits
+CUTE = ["cute", "Awww", "babyduckgifs", "rarepuppers", "cutecats"]
+DUCK = ["duck", "babyduckgifs"]
+FERRETS = ["ferret", "ferrets", "FerretsGoneWild"]
 MEMES = ["dankmemes", "memes"]
-RETRO = ["oldschoolcool", "thewaywewere"]
+PHOTOS = ["Nikon", "streetphotography", "analog", "photographs", "mobilephotography",
+    "itookapicture", "wildlifephotography", "astrophotography", "retouching", "birdpics",
+    "Astronomy", "LandscapePhotography", "pics", "urbanexploration", "japanpics",
+    "foodphotography", "blackandwhite", "fashionphotography", "shootingcars", "nightphotography",
+    "AbstractPhotos", "macro", "earthporn"]
+WALLPAPERS = ["wallpaper", "iphonewallpapers", "MinimalWallpaper", "topwalls",
+    "WQHD_Wallpaper", "iWallpaper", "WidescreenWallpaper"]
+
+# Art Subreddits
+CONCEPT = ["imaginaryaetherpunk", "imaginarycityscapes", "imaginarycolorscapes", "imaginarymindscapes", "art",
+    "smypatheticmonsters", "conceptart"]
+CHARACTER = ["imaginarycharacters", "characterart", "characterdrawing", "dndart"]
+PIXEL = ["pixelart", "pixelartwallpapers", "animatedpixelart"]
+
+MAIN = [CUTE, DUCK, FERRETS, MEMES, PHOTOS, WALLPAPERS]
+ART = [CONCEPT, CHARACTER, PIXEL]
 
 # NSFW Subreddits
 FOUR_K = ["Hegre", "HighResASS", "HighResNSFW", "NSFW_Wallpaper", "UHDnsfw"]
@@ -30,38 +48,53 @@ ASS = ["AssOnTheGlass", "AssReveal", "AssholeBehindThong", "ButtsAndBareFeet", "
     "HighResASS", "HungryButts", "Mooning", "SnakeButt", "TheUnderbun",
     "Upshorts", "ass", "assinthong", "asstastic", "beautifulbutt",
     "bigasses", "booty", "brunetteass", "datgap", "girlsinleggings",
-    "girlsinyogapants", "hugeass", "paag", "pawg", "facedownassup"]
-# ANAL
-# BBW
-# BDSM
-# BLOWJOB
-# BOOBS
-# BOTTOMLESS
-# COSPLAY
-# CUNNI
-# CUMSHOTS
-# DEEPTHROAT
-# DICK
-# EBONY
-# FACIALS
-# FEET
-# FEMDOM
-# FUTA
-# GAY
+    "girlsinyogapants", "hugeass", "paag", "pawg", "facedownassup", "bigblackasses"]
+ANAL = ["AnalGW", "MasterOfAnal", "NotInThePussy", "anal",
+    "analinsertions", "assholegonewild", "buttsthatgrip"]
+BBW = ["BBW", "BBW_Chubby", "GoneWildPlus", "PerkyChubby",
+    "chubby", "gonewildcurvy"]
+BDSM = ["BDSMGW", "BDSM_NoSpam", "Bondage", "Spanking", "bdsm", "ropeart"]
+BLOWJOB = ["AsianBlowjobs", "Blowjobs", "OralCreampie", "SwordSwallowers",
+    "blowjobsandwich", "AmateurDeepthroat", "DeepthroatTears", "deepthroat"]
+BOOBS = ["AreolasGW", "BestTits", "BigBoobsGW", "BigBoobsGonewild", "BiggerThanYouThought",
+    "Boobies", "BoobsBetweenArms", "BustyNaturals", "BustyPetite", "Nipples",
+    "PerfectTits", "PiercedNSFW", "Stacked", "TheHangingBoobs", "TheUnderboob",
+    "TinyTits", "Titties", "TittyDrop", "bigboobs", "boobbounce", "boobgifs",
+    "boobs", "burstingout", "fortyfivefiftyfive", "ghostnipples", "homegrowntits",
+    "hugeboobs", "naturaltitties", "pokies", "smallboobs", "tits"]
+BOTTOMLESS = ["Bottomless", "nopanties", "upskirt"]
+COSPLAY = ["CosplayBoobs", "CosplayLewd", "gwcosplay", "nsfwcosplay"]
+CUNNI = ["CunnilingusSelfie", "cunnilingus"]
+CUMSHOTS = ["GirlsFinishingTheJob", "amateurcumsluts", "bodyshots", "cumfetish", "cumontongue",
+    "cumshots", "facialcumshots", "pulsatingcumshots", "unexpectedcum"]
+DICK = ["penis", "cock", "bulges", "twinks", "ThickDick", "MassiveCock", "Blackdick",
+    "blackcock", "bigblackcocks"]
+DOUBLE_P = ["Techical_DP", "doublepenetration"]
+FEET = ["ButtsAndBareFeet", "Feet_NSFW", "Feetup", "FootFetish", "legsup", "rule34feet"]
+FEMDOM = ["Femdom", "femdom", "hentaifemdom"]
+FUTA = ["FutanariHentai", "Rule34_Futanari", "cutefutanari"]
+GAY = ["CuteGuyButts", "GayGifs", "ManSex", "NSFW_GAY", "broslikeus", "bulges",
+    "gaybears", "gaynsfw", "gayotters", "jockstraps", "ladybonersgw"]
 GROUPS = ["GroupOfNudeGirls", "groupsex"]
-# LESBIANS
-# MILF
-# ORAL
-# PUBLIC
-# PUSSY
-# REAL_GIRLS
-# REDHEADS
+LESBIANS = ["HDLesbianGifs", "Lesbian_gifs", "StraightGirlsPlaying", "girlskissing",
+    "amateurlesbians", "lesbians", "mmgirls", "scissoring"]
+MILF = ["AgedBeauty", "HotAsianMilfs", "blackmilfs", "obsf", "MILFs", "Milfie",
+    "amateur_milfs", "cougars", "maturemilf", "milf"]
+PUBLIC = ["ChangingRooms", "Flashing", "FlashingAndFlaunting", "FlashingGirls",
+    "NSFW_Outdoors", "NotSafeForNature", "NudeInPublic", "PublicFlashing", "WoodNymphs",
+    "bitchinbubba", "casualnudity", "exposedinpublic", "girlsflashing", "gwpublic",
+    "holdthemoan", "publicplug", "snowgirls"]
+PUSSY = ["GodPussy", "Innies", "LabiaGW", "LipsThatGrip", "MoundofVenus", "PerfectPussies",
+    "PussyFlashing", "PussyMound", "grool", "legsup", "peachlips", "pelfie", "pussy",
+    "rearpussy", "spreadeagle", "ButterflyWings", "DangleAndJingle"]
+REAL_GIRLS = ["RealGirls", "Nude_Selfie", "EbonyGirls", "ebonyamateurs"]
 RULE_34 = ["Rule_34", "WesternHentai", "rule34", "rule34cartoons", "rule34_ass",
-    "Rule34Overwatch", "Overwatch_Porn", "Rule34LoL"]
-# SQUIRT
+    "Rule34Overwatch", "Overwatch_Porn", "Rule34LoL", "2booty", "ecchi",
+    "animeecchigifs", "animenudefilters", "doujinshi", "dvansfw",
+    "ecchigifs", "dota2smut", "fitdrawngirls", "healsluts", "hentaimanga",
+    "imaginaryboners", "pantsu", "sukebei", "waifusgonewild", "wholesomehentai"]
 THIGHS = ["ThickThighs", "Thigh", "datgap", "leggingsgonewild", "legs",
     "legsup", "theratio", "thighhighs"]
-# THREESOME
 TRANS = ["GoneWildTrans"]
 WILD = ["ArtGW", "AsiansGoneWild", "BigBoobsGW", "BigBoobsGonewild", "GWCouples",
     "GWNerdy", "GirlsWithBikes", "GoneWildSmiles", "LingerieGW", "PetiteGoneWild",
@@ -69,3 +102,7 @@ WILD = ["ArtGW", "AsiansGoneWild", "BigBoobsGW", "BigBoobsGonewild", "GWCouples"
     "dirtysmall", "gonewild", "gonewildcolor", "gonewildcouples", "gonewildcurvy",
     "gwpublic", "librarygirls", "workgonewild"]
 YIFF = ["Hyiff", "Yiffbondage", "femyiff", "yiff", "yiffgif"]
+
+NSFW = [FOUR_K, AHEGAO, ASS, ANAL, BBW, BDSM, BLOWJOB, BOOBS, BOTTOMLESS, COSPLAY, CUNNI, CUMSHOTS,
+    DICK, DOUBLE_P, FEET, FEMDOM, FUTA, GAY, GROUPS, LESBIANS, MILF, PUBLIC, PUSSY, REAL_GIRLS,
+    RULE_34, THIGHS, TRANS, WILD, YIFF]
--- reddit/reddit.py
+++ reddit/reddit.py
@@ -12,24 +12,261 @@ _ = Translator("reddit", __file__)
 
 @cog_i18n(_)
 class reddit(Core):
-    """Send random images from subreddits."""
+    """Send random images from random subreddits."""
 
-    # NSFW Commands
-    @commands.command(aliases=["butt", "booty"])
-    async def ass(self, ctx):
-        """Sends an ass image from a random subreddit."""
+    # Cog Commands
+    @commands.command()
+    async def redditversion(self, ctx):
+        """Get version number of the installed Reddit cog."""
 
-        await self._send_msg(ctx, _("ass"), sub.ASS)
+        await self._version_msg(ctx, self.__version__, self.__author__)
+
+    # Group Subreddit Commands
+    @commands.command()
+    async def mainrandom(self, ctx):
+        """Sends a random image from the main subreddits."""
+
+        await self._send_msg(ctx, _("main random"), sub.MAIN)
     
-    # SFW Commands
+    @commands.command()
+    async def artrandom(self, ctx):
+        """Sends a random image from the art subreddits."""
+
+        await self._send_msg(ctx, _("art random"), sub.ART)
+
+    @commands.command()
+    async def nsfwrandom(self, ctx):
+        """Sends a random image from the NSFW subreddits."""
+
+        await self._send_msg(ctx, _("nsfw random"), sub.NSFW)
+
+    # Main Subreddit Commands
+    @commands.command()
+    async def cute(self, ctx):
+        """Sends a cute image from a random subreddit."""
+
+        await self._send_msg(ctx, _("cute"), sub.CUTE)
+
+    @commands.command()
+    async def duck(self, ctx):
+        """Sends a duck image from a random subreddit."""
+
+        await self._send_msg(ctx, _("duck"), sub.DUCK)
+
+    @commands.command()
+    async def ferrets(self, ctx):
+        """Sends a ferret image from a random subreddit."""
+
+        await self._send_msg(ctx, _("ferrets"), sub.FERRETS)
+
     @commands.command(aliases=["meme", "dank"])
     async def memes(self, ctx):
         """Sends a meme from a random subreddit."""
 
         await self._send_msg(ctx, _("memes"), sub.MEMES)
 
-    @commands.command(aliases=["vintage"])
-    async def retro(self, ctx):
-        """Sends a retro image from a random subreddit."""
+    @commands.command()
+    async def photos(self, ctx):
+        """Sends an image from a random photography subreddit."""
 
-        await self._send_msg(ctx, _("retro"), sub.RETRO)
+        await self._send_msg(ctx, _("photos"), sub.PHOTOS)
+
+    @commands.command()
+    async def wallpapers(self, ctx):
+        """Sends a wallpaper from a random subreddit."""
+
+        await self._send_msg(ctx, _("wallpapers"), sub.WALLPAPERS)
+
+    # Art Subreddit Commands
+    @commands.command()
+    async def concept(self, ctx):
+        """Sends concept art from a random subreddit."""
+
+        await self._send_msg(ctx, _("concept"), sub.CONCEPT)
+
+    @commands.command()
+    async def character(self, ctx):
+        """Sends character art from a random subreddit."""
+
+        await self._send_msg(ctx, _("character"), sub.CHARACTER)
+
+    @commands.command()
+    async def pixel(self, ctx):
+        """Sends pixel art from a random subreddit."""
+
+        await self._send_msg(ctx, _("pixel"), sub.PIXEL)
+
+    # NSFW Subreddit Commands
+    @commands.command(name="4k", aliases="4K")
+    async def four_k(self, ctx):
+        """Sends a 4k image from a random subreddit."""
+
+        await self._send_msg(ctx, _("4k"), sub.FOUR_K)
+
+    @commands.command()
+    async def ahegao(self, ctx):
+        """Sends an ahegao image from a random subreddit."""
+
+        await self._send_msg(ctx, _("ahegao"), sub.AHEGAO)
+
+    @commands.command(aliases=["butt", "booty"])
+    async def ass(self, ctx):
+        """Sends an ass image from a random subreddit."""
+
+        await self._send_msg(ctx, _("ass"), sub.ASS)
+
+    @commands.command()
+    async def anal(self, ctx):
+        """Sends an anal image from a random subreddit."""
+
+        await self._send_msg(ctx, _("anal"), sub.ANAL)
+
+    @commands.command()
+    async def bbw(self, ctx):
+        """Sends some bbw images."""
+
+        await self._send_msg(ctx, _("bbw"), sub.BBW)
+
+    @commands.command()
+    async def bdsm(self, ctx):
+        """Sends some bdsm from random subreddits."""
+
+        await self._send_msg(ctx, _("bdsm"), sub.BDSM)
+
+    @commands.command(aliases=["blowjobs", "fellatio"])
+    async def blowjob(self, ctx):
+        """Sends some blowjob images/gifs from random subreddits."""
+
+        await self._send_msg(ctx, _("blowjob"), sub.BLOWJOB)
+
+    @commands.command(aliases=["boob", "boobies", "tits", "titties", "breasts", "breast"])
+    async def boobs(self, ctx):
+        """Sends some boobs images from random subreddits."""
+
+        await self._send_msg(ctx, _("boobs"), sub.BOOBS)
+
+    @commands.command()
+    async def bottomless(self, ctx):
+        """Sends some bottomless images from random subreddits."""
+
+        await self._send_msg(ctx, _("bottomless"), sub.BOTTOMLESS)
+
+    @commands.command()
+    async def cosplay(self, ctx):
+        """Sends some nsfw cosplay images from random subreddits."""
+
+        await self._send_msg(ctx, _("nsfw cosplay"), sub.COSPLAY)
+
+    @commands.command(aliases=["cunni", "pussyeating"])
+    async def cunnilingus(self, ctx):
+        """Sends some cunnilingus images from random subreddits."""
+
+        await self._send_msg(ctx, _("cunnilingus"), sub.CUNNI)
+
+    @commands.command(aliases=["cum", "cums", "cumshots"])
+    async def cumshot(self, ctx):
+        """Sends some cumshot images/gifs from random subreddits."""
+
+        await self._send_msg(ctx, _("cumshot"), sub.CUMSHOTS)
+
+    @commands.command(aliases=["cock"])
+    async def dick(self, ctx):
+        """Sends some dicks images from random subreddits."""
+
+        await self._send_msg(ctx, _("dick"), sub.DICK)
+
+    @commands.command(aliases=["doublep"])
+    async def doublepenetration(self, ctx):
+        """Sends some double penetration images/gifs from random subreddits."""
+
+        await self._send_msg(ctx, _("double penetration"), sub.DOUBLE_P)
+
+    @commands.command()
+    async def feet(self, ctx):
+        """Sends some feet images from random subreddits."""
+
+        await self._send_msg(ctx, _("feet"), sub.FEET)
+
+    @commands.command()
+    async def femdom(self, ctx):
+        """Sends some femdom images from random subreddits."""
+
+        await self._send_msg(ctx, _("femdom"), sub.FEMDOM)
+
+    @commands.command(aliases=["futanari"])
+    async def futa(self, ctx):
+        """Sends some futa images from random subreddits."""
+
+        await self._send_msg(ctx, _("futa"), sub.FUTA)
+
+    @commands.command(aliases=["gayporn"])
+    async def gay(self, ctx):
+        """Sends some gay porn from random subreddits."""
+
+        await self._send_msg(ctx, _("gay porn"), sub.GAY)
+
+    @commands.command(aliases=["groups", "nudegroup", "nudegroups"])
+    async def group(self, ctx):
+        """Sends some groups nudes from random subreddits."""
+
+        await self._send_msg(ctx, "groups nudes", sub.GROUPS)
+
+    @commands.command(aliases=["lesbians"])
+    async def lesbian(self, ctx):
+        """Sends some lesbian gifs or images from random subreddits."""
+
+        await self._send_msg(ctx, _("lesbian"), sub.LESBIANS)
+
+    @commands.command(aliases=["milfs"])
+    async def milf(self, ctx):
+        """Sends some milf images from random subreddits."""
+
+        await self._send_msg(ctx, _("milf"), sub.MILF)
+
+    @commands.command()
+    async def public(self, ctx):
+        """Sends some public nude images from random subreddits."""
+
+        await self._send_msg(ctx, _("public nude"), sub.PUBLIC)
+
+    @commands.command(aliases=["vagina"])
+    async def pussy(self, ctx):
+        """Sends some pussy images from random subreddits."""
+
+        await self._send_msg(ctx, _("pussy"), sub.PUSSY)
+
+    @commands.command()
+    async def realgirls(self, ctx):
+        """Sends some real girls images from random subreddits."""
+
+        await self._send_msg(ctx, _("real nudes"), sub.REAL_GIRLS)
+
+    @commands.command(aliases=["r34"])
+    async def rule34(self, ctx):
+        """Sends some rule34 images from random subreddits."""
+
+        await self._send_msg(ctx, _("rule34"), sub.RULE_34)
+
+    @commands.command(aliases=["thighs", "legs"])
+    async def thigh(self, ctx):
+        """Sends some thighs images from random subreddits."""
+
+        await self._send_msg(ctx, _("thigh"), sub.THIGHS)
+
+    @commands.command()
+    async def trans(self, ctx):
+        """Sends some trans from random subreddits."""
+
+        await self._send_msg(ctx, _("trans"), sub.TRANS)
+
+    @commands.command(aliases=["wild", "gwild"])
+    async def gonewild(self, ctx):
+        """Sends some gonewild images from random subreddits."""
+
+        await self._send_msg(ctx, _("gonewild"), sub.WILD)
+
+    @commands.command(aliases=["yiffs"])
+    async def yiff(self, ctx):
+        """Sends some yiff images from random subreddits."""
+
+        await self._send_msg(ctx, _("yiff"), sub.YIFF)
