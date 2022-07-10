4465c31dfc3a23bab5875b567ea3fd15928b4b1b
DavorMajdandzic <davormajmusic@gmail.com>
2022-07-10 09:05:47 -0400

    edit

--- kapptools/__init__.py
+++ kapptools/__init__.py
@@ -1,13 +1,4 @@
-import json
-from pathlib import Path
-
-from redbot.core.bot import Red
-
 from .kapptools import kapptools
-
-with open(Path(__file__).parent / "info.json") as fp:
-    __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
-
 
 def setup(bot):
     bot.add_cog(kapptools(bot))
